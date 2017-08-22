e7d90cc6ae133971619aeb82434d9a88466ce704
Ryan Andersen <ryan.andersen05@gmail.com>
2017-08-22 00:02:23 -0400

    Blank screen and a couple scripts started

---
+++ Assets/Scripts/GameOverseer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverseer : MonoBehaviour {
+    #region static variables
+    private static GameOverseer instance;
+
+    public static GameOverseer Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = GameObject.FindObjectOfType<GameOverseer>();
+            }
+            return instance;
+        }
+    }
+
+    #endregion static varaibles
+
+    #region main variables
+
+    #endregion main variables
+
+
+    #region monobehaviour methods
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion monobehaviour methods
+}
---
+++ Assets/Scripts/UIScripts/MainGameUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// The primary HUD of the game. Holds references to various UI elements for other scripts to reference
+/// </summary>
+public class MainGameUI : MonoBehaviour {
+    #region static variables
+    private static MainGameUI instance;
+
+    public static MainGameUI Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = GameObject.FindObjectOfType<MainGameUI>();
+            }
+            return instance;
+        }
+    }
+    #endregion static variables
+
+    #region monobehaviour methods
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion monobehaviour methods
+}
