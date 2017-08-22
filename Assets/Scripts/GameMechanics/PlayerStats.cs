f1df942a5e95b070af42a13e5fc4a5d530e7c5be
Ryan Andersen <ryan.andersen05@gmail.com>
2017-08-22 01:12:17 -0400

    basic player stats script

---
+++ Assets/Scripts/GameMechanics/PlayerStats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is a reference to the player's stats. It is used to store any inventory or general stats of the player
+/// Specific properties such as speed and jump height will NOT be stored here
+/// </summary>
+public class PlayerStats : MonoBehaviour {
+    public float maxHealth;
+    public float currentHealth{ get; private set;}
+
+}
