b70d044f7712bf81c6a9bf0766044df53acb95bb
Ryan Andersen <ryan.andersen05@gmail.com>
2017-08-23 00:47:18 -0400

    added some scripts

---
+++ Assets/Scripts/GameMechanics/CharacterMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+    #region main variables
+    public float runSpeed = 7.5f;
+    public float walkSpeed = 4;
+
+    private float currentSpeed;
+    #endregion main variables
+
+    #region monobehaviour methods
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+    #endregion monobehaviour methods
+
+    private void UpdateSpeed()
+    {
+
+    }
+
+}
