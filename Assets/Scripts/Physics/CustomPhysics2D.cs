4541b0b36f79011a6f4c6f97bea9dc9a713d9480
Ryan Andersen <ryan.andersen05@gmail.com>
2017-08-23 00:54:02 -0400

    position updates to Custom physics

--- Assets/Scripts/Physics/CustomPhysics2D.cs
+++ Assets/Scripts/Physics/CustomPhysics2D.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomPhysics2D : MonoBehaviour {
+    public const float GRAVITY_CONST = 9.8f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float gravityScale;
+    public Vector2 velocity;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        UpdatePositionBasedOnVelocity();
+    }
+
+    /// <summary>
+    /// This strictly moves the transform  based on the velocity. Other checks will be necessary to detect
+    /// collisions
+    /// </summary>
+    private void UpdatePositionBasedOnVelocity()
+    {
+        Vector3 positionOffset = velocity * Time.deltaTime;
+        transform.position = transform.position + positionOffset;
+    }
 }
