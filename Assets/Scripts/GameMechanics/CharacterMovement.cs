9ecff3dc96a3ec2c911e3e0e7c64244734c53241
Ryan Andersen <ryan.andersen05@gmail.com>
2017-08-23 01:06:38 -0400

    Movement variable updates

--- Assets/Scripts/GameMechanics/CharacterMovement.cs
+++ Assets/Scripts/GameMechanics/CharacterMovement.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 [RequireComponent(typeof(CustomPhysics2D))]
 public class CharacterMovement : MonoBehaviour {
     #region main variables
+    [Tooltip("The desired speed when the character is running")]
     public float runSpeed = 7.5f;
+    [Tooltip({"The desired speed when a character is walking. Should typically be slower than the run speed")]
     public float walkSpeed = 4;
+
+    [Tooltip("The height at which the character should be able to jump when shooting for a full jump")]
+    public float jumpHeight = 5;
+    [Tooltip("The time it should take in order to reach the desired jump height")]
+    public float timeToMaxHeight = 1;
 
     private float currentSpeed;
     private CustomPhysics2D rigid;
+    /// <summary>
+    /// This is the horizontal input that was entered by the player or AI to indicate horizontal movement
+    /// </summary>
+    private float hInput;
     #endregion main variables
 
     #region monobehaviour methods
