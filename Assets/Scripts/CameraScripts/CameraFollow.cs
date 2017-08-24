69e4e7b8f4d4b6b71c9da4b84003ada29551b2f6
Ryan Andersen <ryan.andersen05@gmail.com>
2017-08-24 01:02:08 -0400

    Adding a camera follow script

---
+++ Assets/Scripts/CameraScripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public float cameraLerpSpeed;
+
+    private Transform targetTransform;
+    private Vector2 targetOffset;
+
+    /// <summary>
+    /// On start up we set the targetTransform to be the cameras parent
+    /// </summary>
+    private void Awake()
+    {
+        targetTransform = this.transform.parent;
+        if (targetTransform)
+        {
+            targetOffset = targetTransform.position - this.transform.position;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (!targetTransform) return;
+    }
+}
