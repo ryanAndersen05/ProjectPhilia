16bd8ce48bb38636f27e403425b068083a1104d6
Ryan Andersen <ryan.andersen05@gmail.com>
2017-08-24 02:33:22 -0400

    Updates to the movement script to allow the player to change directions

--- Assets/Scripts/GameMechanics/CharacterMovement.cs
+++ Assets/Scripts/GameMechanics/CharacterMovement.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour {
     #region main variables
     private const float RUN_THRESHOLD = .7f;
+    private const bool DIRECTION_RIGHT = true;
+    private const bool DIRECTION_LEFT = false;
 
+    [Header("Movement Variables")]
     [Tooltip("The desired speed when the character is running")]
     public float runSpeed = 7.5f;
     [Tooltip("The desired speed when a character is walking. Should typically be slower than the run speed")]
     public float walkSpeed = 4;
-
     [Tooltip("The acceleration of the character while they are on the ground")]
     public float groundAcceleration = 25f;
+
+    [Header("Jump Varaibles")]
     [Tooltip("The height at which the character should be able to jump when shooting for a full jump")]
     public float jumpHeight = 5;
     [Tooltip("The time it should take in order to reach the desired jump height")]
     public float timeToMaxHeight = 1;
+
+    [Header("Flip Sprite Variables")]
+    [Tooltip("Mark this variable true if the sprite is allowed to change directions based on the input")]
+    public bool spriteFlipBasedOnInput = true;
+    [Tooltip("Represents the current direction of the sprite. If the sprite is facing right, this will be true. NOTE: If the character is not facing right by default you can change its orientation in the inspector through the Sprite Renderer")]
+    public bool isFacingRight = true;
 
     private float currentSpeed;
     private CustomPhysics2D rigid;
@@ -37,11 +47,12 @@ public class CharacterMovement : MonoBehaviour {
     private void Update()
     {
         UpdateSpeed();
+        UpdateSpriteDirection();
     }
 
     private void OnValidate()
     {
-        
+        SetSpriteDirection(this.isFacingRight);
     }
     #endregion monobehaviour methods
 
@@ -49,6 +60,7 @@ public class CharacterMovement : MonoBehaviour {
     {
         this.hInput = hInput;
     }
+
 
     private void UpdateSpeed()
     {
@@ -58,6 +70,36 @@ public class CharacterMovement : MonoBehaviour {
         rigid.velocity = new Vector2(Mathf.MoveTowards(rigid.velocity.x, goalSpeed, Time.deltaTime * groundAcceleration), rigid.velocity.y);
     }
 
+    /// <summary>
+    /// Use this method in the update loop to check if the sprite should change directions based on the
+    /// horizontal input
+    /// </summary>
+    private void UpdateSpriteDirection()
+    {
+        if (!spriteFlipBasedOnInput || Mathf.Abs(this.hInput) < .1f) return;
+
+        if (this.hInput > 0 && !this.isFacingRight)
+        {
+            SetSpriteDirection(DIRECTION_RIGHT);
+        }
+        else if (this.hInput < 0 && this.isFacingRight)
+        {
+            SetSpriteDirection(DIRECTION_LEFT);
+        }
+    }
 
 
+    public void SetSpriteDirection (bool isRight)
+    {
+        this.isFacingRight = isRight;
+
+        if (isFacingRight)
+        {
+            this.transform.localScale = new Vector3(Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+        }
+        else
+        {
+            this.transform.localScale = new Vector3(-Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+        }
+    }
 }
