6ea245657538b6eb177aa1b43632ed4af2aeac3e
Ryan Andersen <ryan.andersen05@gmail.com>
2017-08-25 00:38:28 -0400

    gravity added into game

--- Assets/Scripts/Physics/CustomPhysics2D.cs
+++ Assets/Scripts/Physics/CustomPhysics2D.cs
@@ -7,12 +7,19 @@ public class CustomPhysics2D : MonoBehaviour {
 
     [Tooltip("This controls the scale of the gravity we are applying")]
     public float gravityScale = 1;
+
+    [Tooltip("The vector that represents the direction that the force of cravity will be applied")]
+    public Vector2 gravityDirection = Vector2.down;
+
     public Vector2 velocity { get; set; }
 
     /// <summary>
-    /// This is the value of the gravity when 
+    /// This is the value of the gravity after adjusting for everything but gravity-scale
+    /// Please be sure to apply the gravity scale to ensure the correct calculations are acheieved
     /// </summary>
     private float gravityValue = GRAVITY_CONST;
+
+    private float terminalVelocity = 15f;
 
     private void Awake()
     {
@@ -24,6 +31,11 @@ public class CustomPhysics2D : MonoBehaviour {
         UpdatePositionBasedOnVelocity();
     }
 
+    private void FixedUpdate()
+    {
+        UpdateVelocityFromGravity();
+    }
+
     /// <summary>
     /// This strictly moves the transform  based on the velocity. Other checks will be necessary to detect
     /// collisions
@@ -32,5 +44,17 @@ public class CustomPhysics2D : MonoBehaviour {
     {
         Vector3 positionOffset = velocity * Time.deltaTime;
         transform.position = transform.position + positionOffset;
+    }
+
+    private void UpdateVelocityFromGravity()
+    {
+        float adjustVelocitySpeed = Time.fixedDeltaTime * gravityScale * gravityValue;
+
+        velocity += adjustVelocitySpeed * gravityDirection;
+
+        if (velocity.y < -terminalVelocity)
+        {
+            velocity = new Vector2(velocity.x, -terminalVelocity);
+        }
     }
 }
