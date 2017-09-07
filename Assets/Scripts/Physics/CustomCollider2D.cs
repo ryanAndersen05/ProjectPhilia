f29f6b65d3cfa324a49839b24022a5e9ba616352
Ryan Andersen <ryan.andersen05@gmail.com>
2017-09-06 23:44:59 -0400

    Added a new custom collider

---
+++ Assets/Scripts/Physics/CustomCollider2D.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class CustomCollider2D : MonoBehaviour {
+    #region main variables
+    public BoxCollider2D boxCollider;
+    #endregion main variables
+
+    #region monobehaviour methods
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+
+    private void Update()
+    {
+        
+    }
+    #endregion monobehaviour methods
+
+
+    private BoxCornerStruct GetCurrentColliderCorners()
+    {
+        BoxCornerStruct boxCorners = new BoxCornerStruct();
+
+        boxCorners.bottomRight = new Vector2(boxCollider.bounds.max.x, boxCollider.bounds.min.y);
+        boxCorners.bottomLeft = boxCollider.bounds.min;
+        boxCorners.topRight = boxCollider.bounds.max;
+        boxCorners.topLeft = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.max.y);
+
+        return boxCorners;
+    } 
+
+
+    private struct BoxCornerStruct
+    {
+        public Vector2 bottomRight;
+        public Vector2 bottomLeft;
+        public Vector2 topRight;
+        public Vector2 topLeft;
+    }
+}
